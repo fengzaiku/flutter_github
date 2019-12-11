e2b40d46f7c1908fc55121b521923cddbc09a7b8
lianxunshuai <lianxunshuai@okay.cn>
2019-12-11 19:02:57 +0800

    修改doi

--- lib/https/index.dart
+++ lib/https/index.dart
@@ -1,14 +1,31 @@
 //import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:flutter_github/https/response/response_data.dart';
 
 class Http {
   Dio dio = Dio();
 
-  Future request(String path,
+  Future<ResponseData> request(String path,
       {data, Map params, Options options}) async {
     Response response;
     try {
       response = await dio.request(path, data: data, options: options);
+    } catch (e) {}
+  }
+
+  Future<ResponseData> post(
+    String path, {
+    data,
+    Map<String, dynamic> params,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onSendProgress,
+    ProgressCallback onReceiveProgress,
+  }) async {
+
+    Response response;
+    try {
+      response = await dio.post(path, data: data);
     } catch (e) {}
   }
 }
