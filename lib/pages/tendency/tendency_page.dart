d2810ce83cfb5d5ad952672acfa8dfa63fd2fa17
lianxunshuai <xunaiya@sina.com>
2019-12-25 01:11:19 +0800

    解析页面

--- lib/pages/tendency/tendency_page.dart
+++ lib/pages/tendency/tendency_page.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_github/common/const/api.dart';
 import 'package:flutter_github/common/utils/http.dart';
+import 'package:flutter_github/model/TrendingRepoModel.dart';
 import 'package:flutter_github/pages/tendency/widget/tendency_header.dart';
 import 'package:flutter_github/store/app_state.dart';
 import 'package:flutter_github/store/async_reducers/trend_reducer.dart';
+import 'package:flutter_github/utils/html_utils.dart';
 import 'package:flutter_github/widget/reposition_item.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -26,7 +28,12 @@ class _TendencyPageWidgetState extends State<TendencyPageWidget>{
 //    getTrendingList
   }
   Future<void> _onRefresh() async{
+    List<TrendingRepoModel> trendingRepoModels;
     var result = await http.request(Api.getTrending("daily", "Java"),null, Options(contentType: "text/plain; charset=utf-8", method: "get"));
+    if(result != null){
+      trendingRepoModels = await HtmlUtils.formatTrendingHtml(result);
+    }
+
   }
 
   @override
--- lib/utils/html_utils.dart
+++ lib/utils/html_utils.dart
@@ -7,26 +7,44 @@ class HtmlUtils {
     Document document = parse(html);
 
     List<TrendingRepoModel> list = List();
-    List<Element> articles = document.querySelectorAll("article.Box-row");
+    List articles = document.querySelectorAll("article.Box-row");
 
     if(articles.isNotEmpty){
 //      list = List.generate(articles.length, generator);
-      list = articles.map((element){
-        return TrendingRepoModel(
-          fullName:"",
-          url:"",
-          description: element.querySelector('p.my-1').innerText,
-          language: element.querySelector('span[itemprop="programmingLanguage"]').innerText,
-          meta:"",
-          contributors:"",
-          contributorsUrl:"",
-          starCount: element.querySelector('svg[aria-label="star"]').nextSibling.trim(),
-          forkCount: element.querySelector('svg[aria-label="repo-forked"]').nextSibling.trim(),
-          name:"",
-          reposName:"",
-        );
-      });
-    }
+      list.addAll(articles.map((element){
+        RegExp hrefExp = new RegExp("(?!\{href\:)(\/[a-zA-Z0-9]+)+");
+        String hrefString = element.querySelector('h1>a').attributes.toString();
 
+
+        String url = hrefExp.firstMatch(hrefString)[0];
+        List<String> contributors = List();
+        List images = element.querySelectorAll('a[data-hovercard-type="user"]');
+        images.forEach((curElement){
+          RegExp srcExp = new RegExp("https?.*\=\d+");
+//          srcExp.stringMatch(input)
+          var imgEle = curElement.querySelector("img.avatar");
+          String srcString = imgEle.attributes.toString();
+          print("srcString----------------------------${srcString}");
+          print("srcExp.firstMatch(srcString)[0]----------------------------${srcExp.stringMatch(srcString)}");
+//          print("srcExp.firstMatch(srcString)[0]----------------------------${srcExp.firstMatch(srcString)[0]}");
+//          contributors.add(srcExp.firstMatch(srcString)[0]);
+        });
+        print("ele.attributes.href----------------------------${contributors}");
+        return TrendingRepoModel.fromJson({
+          "fullName":url.substring(1, url.length),
+          "url": url,
+          "description": element.querySelector('p.my-1').innerText,
+          "language": element.querySelector('span[itemprop="programmingLanguage"]').innerText,
+          "meta": element.querySelector(".float-sm-right").lastChild.textContent,
+          "contributors": contributors,
+          "contributorsUrl":contributors[1],
+          "starCount": element.querySelector('svg[aria-label="star"]').nextSibling.textContent,
+          "forkCount": element.querySelector('svg[aria-label="repo-forked"]').nextSibling.textContent,
+          "name":url.split('/')[1],
+          "reposName":url.split('/')[2],
+        });
+      }));
+    }
+    return list;
  }
 }
